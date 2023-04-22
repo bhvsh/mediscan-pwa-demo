b6344aca9877d6815c6d4ccb153e92bf286a4ba3
Bhavish B S <75534159+bhvsh@users.noreply.github.com>
2023-04-22 12:57:54 +0530

    Refactored source code using pylint

---
+++ config.py
@@ -0,0 +1,4 @@
+"""Config file for the project."""
+
+# The URL of the API used for image classification
+API_URL = 'Removed for security reasons'
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import requests
+"""
+This module contains the flask app that runs the web application.
+The app has two routes:
+    1. /: The main page of the web application.
+    2. /result: The result page of the web application.
+    
+    The main page contains a form that allows the user to upload an image.
+    The result page contains the result of the image classification.
+    
+    The app sends a POST request to the API with the image and receives the result.
+    The result is then displayed on the result page.
+
+    The app also saves the image to a file and displays it on the result page.
+        
+    The app is run by running the main.py file.
+"""
 import json
 import base64
-from flask import Flask, render_template, request, redirect, url_for
 import os
+import requests
+from flask import Flask, render_template, request, redirect, url_for
 import config
 
 app = Flask(__name__)
@@ -10,7 +26,8 @@ app = Flask(__name__)
 # direct to main.html
 @app.route('/')
 def main():
-    #  if the image is already in the static folder, delete it
+    """Renders the main page."""
+    # Remove the result.jpg file if it exists
     if os.path.exists('static/result.jpg'):
         os.remove('static/result.jpg')
 
@@ -18,32 +35,33 @@ def main():
 
 @app.route('/result',  methods=['GET'])
 def redirect_to_main():
+    """Redirects the user to the main page if user tries to access result page directly."""
     return redirect(url_for('main'))
 
 # direct to result.html
 @app.route('/result',  methods=['POST'])
 def result():
-    # get the image from the form
+    """Sends the POST request to the API and renders the result page."""
+    # Get the image from the form
     image = request.files['image']
-    # convert image to base64 string
+    # Convert image to base64 string
     encoded_string = base64.b64encode(image.read()).decode('utf-8')
-    # send image to api
+    # Send image to api
     data = {'image': encoded_string}
     headers = {'Content-type': 'application/json'}
-    response = requests.post(config.api_url, data=json.dumps(data), headers=headers)
-    # get the result from api
-    resp = eval(response.text)
-    # get the image from api
+    response = requests.post(config.API_URL, data=json.dumps(data), headers=headers, timeout=10)
+    # Get the result from api
+    resp = json.loads(response.text)
+    # Get the image from api and convert from base64 string to image
     img_display = base64.b64decode(resp['image'][2:-1])
-    # convert img_display to image to send to html
-    # get the mfg date from api
+    # Get the mfg date from api
     mfg_date = resp['mfg_date']
-    # get the exp date from api
+    # Get the exp date from api
     exp_date = resp['expiry_date']
-    # save the image to a file
-    with open("static/result.jpg", "wb") as fh:
-        fh.write(img_display)
-    # direct to result.html
+    # Save the image to a file
+    with open("static/result.jpg", "wb") as result_file:
+        result_file.write(img_display)
+    # Direct to result.html
     return render_template('/result.html',mfg_date=mfg_date, exp_date=exp_date)
 
 if __name__=='__main__':
